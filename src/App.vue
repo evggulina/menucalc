d02184b6cbb21d2ecba5cb9ca8eba8b2df83f616
Гулина Евгения <evg_gulina@mail.ru>
2025-03-13 15:41:53 +0300

    #S работа над стилями в компоненте суточного калоража

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
 
 const kcal = ref(null)
 
@@ -10,51 +10,86 @@ function changeKcal() {
 </script>
 
 
-
-
 <template>
 
-        <div class="title-card">
-            <div class="ps-5 d-flex" >
-                <a href="#" class="link-success">Админка</a>
-            </div>
-
-            <div
-                v-if="kcal > 0"
-                class="d-flex"
-            >
-                <div class="w-100">
-                    <h1 class="ps-3">Меню на неделю на {{ kcal }}</h1>
-                </div>
-
-                <div class="w-25">
-                    <button
-                        class="btn btn-outline-secondary"
-                        @click="changeKcal"> Изменить
-                    </button>
-                </div>
-            </div>
-
-            <form
-                v-else
-                class="d-flex">
-                <div class="w-100">
-                    <h4 class="form-label text-end pe-3">Введите количество калорий в день: </h4>
-                </div>
-
-                <div class="w-25">
-                    <input
-                        type="text"
-                        class="form-control"
-                        v-model.number.lazy="kcal">
-                </div>
-            </form>
+    <div class="title-card ">
+        <div class="p-3">
+            <a href="#" class="btn btn-outline-orange">Админка</a>
         </div>
+
+        <div
+            v-if="kcal>0"
+            class="card-kcal"
+        >
+            <div>
+                <h1>Меню на неделю на {{ kcal }}</h1>
+            </div>
+
+            <div>
+                <button
+                    class="btn btn-outline-orange"
+                    @click="changeKcal"> Изменить
+                </button>
+            </div>
+        </div>
+
+
+        <form
+            v-else
+            class="input-kcal">
+            <div>
+                <h4 class="form-label text-end pe-3">Введите количество калорий в день: </h4>
+            </div>
+
+            <div>
+                <input
+                    type="text"
+                    class="form-control"
+                    v-model.number.lazy="kcal">
+            </div>
+        </form>
+    </div>
+
+
 </template>
 
 
-
-
 <style scoped>
+
+.title-card {
+    display: grid;
+    grid-template-columns: 30% auto;
+    min-height: 100px;
+    background-color: white;
+    border-bottom: 1px solid #e2970c;
+}
+
+.card-kcal, .input-kcal {
+    display: grid;
+    grid-template-columns: 70% 25%;
+    padding-top: 20px;
+}
+
+.card-kcal h1 {
+    font-family: "Baskerville Old Face", serif;
+    font-size: 35px ;
+    font-style: oblique;
+    color: #634308 ;
+}
+
+.btn-outline-orange {
+    --bs-btn-font-weight: 600;
+    --bs-btn-color: var(--color-orange) !important;
+    --bs-btn-bg: var(--bs-white);
+    --bs-btn-border-color: var(--color-orange) !important;
+    --bs-btn-active-color: var(--bs-white);
+    --bs-btn-active-bg: var(--color-orange) !important;;
+    --bs-btn-active-border-color: var(--bs-white);
+    --bs-btn: var(--color-orange) !important;
+    --bs-btn-hover-color: var(--color-orange) !important;
+    --bs-btn-hover-bg: none;
+    --bs-btn-hover-border-color: var(--bs-white);
+}
+
 
 </style>
